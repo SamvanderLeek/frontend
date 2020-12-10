a66e3572e33cc8831044c456781a2a2430fe9d15
SamvanderLeek <74157405+SamvanderLeek@users.noreply.github.com>
2020-12-10 13:24:26 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 console.log("werkt");
+
+/* HAMBURGER MENU */
+
+/* const is een waarde die niet veranderd */
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false; /* let is de nieuwe versie van var */
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+/* BRON: https://www.youtube.com/watch?v=dIyVTjJAkLw&t=350s */
